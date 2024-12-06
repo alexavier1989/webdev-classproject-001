f37423ac48a5097f5d2a3ddd793c753c6f2fed38
Alex Javier <alexavier1989@gmail.com>
2024-12-06 01:53:36 -0500

    feat: adding js functions to comsume quotes api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 //JavaScript
+
+function changeBackgroundByAuthor(){
+    // TODO: Change de background image by quote author
+}
+
+function callFunctionByTime(apiUrl, time) {
+
+    async function fetchData() {
+        try {
+            const response = await fetch(apiUrl);
+
+            if (!response.ok) {
+                throw new Error(`Error in the response: ${response.status} - ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            let quote = document.querySelector(".quote");
+            let author = document.querySelector(".author");
+
+            quote.innerHTML = data[0].quote;
+            author.innerHTML = data[0].author;
+            changeBackgroundByAuthor();
+
+        } catch (error) {
+            console.error('API error: ', error);
+        }
+    }
+
+    fetchData();
+    setInterval(fetchData, time);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("La página se ha cargado completamente");
+    const apiUrl = "https://api.breakingbadquotes.xyz/v1/quotes";
+    callFunctionByTime(apiUrl, 6000);
+})
+
